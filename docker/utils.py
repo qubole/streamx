b79b965ec07ada64c6ef3328cbad06f6fc36c45b
Ashish Sachdeva <ashishs@qubole.com>
2017-06-16 17:02:11 +0530

    changes for roles

--- docker/utils.py
+++ docker/utils.py
@@ -12,8 +12,7 @@ def check_for_required_configs(confs):
     print_help()
     sys.exit(1)
 
-  #required_configs = {"CONNECT_BOOTSTRAP_SERVERS": False, "CONNECT_AWS_ACCESS_KEY": False, "CONNECT_AWS_SECRET_KEY": False}
-  required_configs = {"CONNECT_BOOTSTRAP_SERVERS": False, "CONNECT_CLUSTER_ON_ROLES": False}
+  required_configs = {"CONNECT_BOOTSTRAP_SERVERS": False, "CONNECT_PROPS": False}
   for x in required_configs.keys():
     if x in confs:
        required_configs[x] = True
@@ -22,6 +21,23 @@ def check_for_required_configs(confs):
     if required_configs[x] == False:
       print x +" is required"
       sys.exit(1)
+
+  props = confs['CONNECT_PROPS']
+  props_arr = props.split('!')[:-1]
+  for prop in props_arr:
+      (k,v) = prop.split('=',1)
+      confs[k] = v
+
+  if not confs.has_key('CONNECT_CLUSTER_ON_ROLES'):
+      print "Cluster on Roles property required(either true of false)."
+
+  cluster_on_roles = confs['CONNECT_CLUSTER_ON_ROLES']
+  if cluster_on_roles is False:
+      if (not (confs.has_key('CONNECT_AWS_ACCESS_KEY') and confs.has_key('CONNECT_AWS_SECRET_KEY'))):
+          print "AWS ACCESS and SECRET keys are required when not on Roles."
+          sys.exit(1)
+
+  return confs
 
 def override_connect_configs(confs):
   connect_file = "/usr/local/streamx/config/connect-distributed.properties"
@@ -48,16 +64,19 @@ def override_connect_configs(confs):
 
 def override_hadoop_configs(confs):
   hadoop_conf_file = "/usr/local/streamx/config/hadoop-conf/hdfs-site.xml"
-  #access_key = confs["CONNECT_AWS_ACCESS_KEY"] 
-  #secret_key = confs["CONNECT_AWS_SECRET_KEY"]
   cluster_on_roles = confs["CONNECT_CLUSTER_ON_ROLES"]
   import subprocess
-  #cmd1='sed -i "s:SECRET_KEY_HERE:' + secret_key + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
-  #cmd2='sed -i "s:ACCESS_KEY_HERE:' + access_key + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
-  #subprocess.Popen(cmd1, shell=True).wait()
-  #subprocess.Popen(cmd2, shell=True).wait()
   cmd='sed -i "s:IS_CLUSTER_ON_ROLE:' + cluster_on_roles + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
   subprocess.Popen(cmd, shell=True).wait()
+
+  if cluster_on_roles is False:
+      access_key = confs["CONNECT_AWS_ACCESS_KEY"]
+      secret_key = confs["CONNECT_AWS_SECRET_KEY"]
+
+      cmd1='sed -i "s:SECRET_KEY_HERE:' + secret_key + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
+      cmd2='sed -i "s:ACCESS_KEY_HERE:' + access_key + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
+      subprocess.Popen(cmd1, shell=True).wait()
+      subprocess.Popen(cmd2, shell=True).wait()
 
 def main():
   confs = {}
@@ -68,7 +87,7 @@ def main():
     else:
       print "Ignoring " + x + " as it does not start with CONNECT_"
 
-  check_for_required_configs(confs)
+  confs = check_for_required_configs(confs)
   override_connect_configs(confs)
   override_hadoop_configs(confs)
 
