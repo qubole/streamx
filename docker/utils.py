3aa05d742d82e63b2e38e59d6275316bbf2962cd
ashishsachdeva <ashish.asachdeva@gmail.com>
2017-08-07 10:37:28 +0530

    AWS Roles support

--- docker/utils.py
+++ docker/utils.py
@@ -5,22 +5,24 @@ def print_help():
   CONNECT_BOOTSTRAP_SERVERS
   CONNECT_AWS_ACCESS_KEY
   CONNECT_AWS_SECRET_KEY
-  Cmd : docker run -d --env CONNECT_BOOTSTRAP_SERVERS=public_dns:9092 --env CONNECT_AWS_ACCESS_KEY=xxxxx --env CONNECT_AWS_SECRET_KEY=yyyy qubole/streamx"""
+  Cmd : docker run -d --env CONNECT_BOOTSTRAP_SERVERS=public_dns:9092 --env CONNECT_AWS_ACCESS_KEY=xxxxx --env CONNECT_AWS_SECRET_KEY=yyyy --env CONNECT_CLUSTER_ON_ROLES=True/False qubole/streamx"""
 
 def check_for_required_configs(confs):
   if len(confs) == 0:
     print_help()
     sys.exit(1)
 
-  required_configs = {"CONNECT_BOOTSTRAP_SERVERS": False, "CONNECT_AWS_ACCESS_KEY": False, "CONNECT_AWS_SECRET_KEY": False}
-  for x in required_configs.keys():
-    if x in confs:
-       required_configs[x] = True
+  required_configs = ["CONNECT_BOOTSTRAP_SERVERS", "CONNECT_AWS_ACCESS_KEY", "CONNECT_AWS_SECRET_KEY", "CONNECT_CLUSTER_ON_ROLES"]
+  for x in required_configs:
+    if x not in confs.keys():
+       print x +" is required"
+       sys.exit(1)
 
-  for x in required_configs.keys():
-    if required_configs[x] == False:
-      print x +" is required"
-      sys.exit(1)
+  cluster_on_roles = confs['CONNECT_CLUSTER_ON_ROLES']
+  if cluster_on_roles == "False":
+      if ((not confs['CONNECT_AWS_ACCESS_KEY'].strip()) or (not confs['CONNECT_AWS_SECRET_KEY'].strip())):
+          print "AWS ACCESS and SECRET keys are required when not on Roles."
+          sys.exit(1)
 
 def override_connect_configs(confs):
   connect_file = "/usr/local/streamx/config/connect-distributed.properties"
@@ -47,13 +49,19 @@ def override_connect_configs(confs):
 
 def override_hadoop_configs(confs):
   hadoop_conf_file = "/usr/local/streamx/config/hadoop-conf/hdfs-site.xml"
-  access_key = confs["CONNECT_AWS_ACCESS_KEY"] 
-  secret_key = confs["CONNECT_AWS_SECRET_KEY"]
+  cluster_on_roles = confs["CONNECT_CLUSTER_ON_ROLES"]
   import subprocess
-  cmd1='sed -i "s:SECRET_KEY_HERE:' + secret_key + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
-  cmd2='sed -i "s:ACCESS_KEY_HERE:' + access_key + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
-  subprocess.Popen(cmd1, shell=True).wait()
-  subprocess.Popen(cmd2, shell=True).wait()
+  cmd='sed -i "s:IS_CLUSTER_ON_ROLES:' + cluster_on_roles + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
+  subprocess.Popen(cmd, shell=True).wait()
+
+  if cluster_on_roles == "False":
+    access_key = confs["CONNECT_AWS_ACCESS_KEY"]
+    secret_key = confs["CONNECT_AWS_SECRET_KEY"]
+
+    cmd1='sed -i "s:SECRET_KEY_HERE:' + secret_key + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
+    cmd2='sed -i "s:ACCESS_KEY_HERE:' + access_key + ':g" /usr/local/streamx/config/hadoop-conf/hdfs-site.xml'
+    subprocess.Popen(cmd1, shell=True).wait()
+    subprocess.Popen(cmd2, shell=True).wait()
 
 def main():
   confs = {}
